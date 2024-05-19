ab615026d2e1f7bc2d26b5d46985755950a35d05
Ayush Singh <ayush.ved78@gmail.com>
2024-05-19 22:18:16 +0530

    Time: 1016 ms (27.36%), Space: 20 MB (20.90%) - LeetHub

---
+++ 0792-number-of-matching-subsequences/0792-number-of-matching-subsequences.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        lookup = defaultdict(list)
+        output = 0
+        # create lookup:hash
+        for i,c in enumerate(s):
+            lookup[c].append(i)
+        # perform binary search
+        def bs(lst,i):
+            l,r = 0, len(lst)
+            while l<r:
+                mid = (l+r) // 2
+                if i < lst[mid]:
+                    r = mid
+                else:
+                    l = mid + 1
+            return l
+        # fetch the output
+        for w in words:
+            prev = -1
+            found = True
+            for c in w:
+                tmp = bs(lookup[c],prev)
+                if tmp == len(lookup[c]):
+                    found = False
+                    break
+                else:
+                    prev = lookup[c][tmp]
+            if found:
+                output += 1
+        # return the output       
+        return output
